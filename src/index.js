f7c1c466fe408d38f06e42c5f2fdf176739ff3af
canjeinner-spec <canjeinner@gmail.com>
2025-12-11 09:37:50 +0300

    Add service worker registration in index.js
    Register service worker if supported by the browser.

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .catch(() => {});
+  });
+}
